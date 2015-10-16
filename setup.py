823c52a3a6d4f7d8c5173ac14556f228dece3c2c
Lance Hepler <nlhepler@gmail.com>
2015-10-16 14:50:55 -0700

    Changes to support passing in paths for boost, cmake, and swig

--- setup.py
+++ setup.py
@@ -32,27 +32,37 @@ class MyBuild(dbuild_py):
         buildDir = mkdtemp()
         try:
             cleanAll(self)
-            retcode = Popen(["cmake", "-DCMAKE_BUILD_TYPE=Release", "-DPYTHON_SWIG=1", "-DPYTHON_EXECUTABLE={0}".format(sys.executable), thisDir], cwd=buildDir).wait()
+            env = os.environ.copy()
+            cmake = env.get("CMAKE_COMMAND", "cmake")
+            boost = env.get("Boost_INCLUDE_DIRS", None)
+            swig = env.get("SWIG_COMMAND", None)
+            cmds = [cmake,
+                    "-DCMAKE_BUILD_TYPE=RelWithDebInfo",
+                    "-DPYTHON_SWIG=1",
+                    "-DPYTHON_EXECUTABLE={0}".format(sys.executable)]
+            if boost is not None:
+                cmds.append("-DBoost_INCLUDE_DIRS={0}".format(boost))
+            if swig is not None:
+                cmds.append("-DSWIG_COMMAND={0}".format(swig))
+            cmds.append(thisDir)
+            print("running command: {0}".format(" ".join(cmds)), file=sys.stderr)
+            retcode = Popen(cmds, cwd=buildDir, env=env).wait()
             if (retcode != 0):
                 raise CompileError("failed to configure the pbconsensus with CMake!")
+            print("running command: make", file=sys.stderr)
             retcode = Popen(["make"], cwd=buildDir).wait()
             if (retcode != 0):
                 raise CompileError("failed to compile or link pbconsensus!")
         finally:
             rmtree(buildDir)
-            try:
-                super(MyBuild, self).run()
-            except TypeError:
-                dbuild_py.run(self)
+            try: super(MyBuild, self).run()
+            except TypeError: dbuild_py.run(self)
 
 class MyInstall(dinstall):
     def run(self):
-        try:
-            super(MyInstall, self).run()
-        except TypeError:
-            dinstall.run(self)
-        finally:
-            cleanAll(self)
+        try: super(MyInstall, self).run()
+        except TypeError: dinstall.run(self)
+        finally: cleanAll(self)
 
 class MyDist(Distribution):
     def has_ext_modules(self):
