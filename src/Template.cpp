7fdb37330b9d82ee3d162d80540cf25ef011eb0c
Brett Bowman <bnbowman@gmail.com>
2015-10-25 11:34:01 -0700

    Workaround for another crash due to out-of-bounds mutations

--- src/Template.cpp
+++ src/Template.cpp
@@ -168,6 +168,10 @@ void Template::ApplyMutation(const Mutation& mut)
 
     const size_t i = mut.Start() - start_;
 
+     // TODO(lhepler): The following should never happen, but does.
+     //     find the root cause, fix it, and nuke this line
+     if (i >= tpl_.size()) return;
+
     if (mut.Type == MutationType::INSERTION) {
         if (i > 0) tpl_[i - 1] = cfg_->Populate({tpl_[i - 1].Base, mut.Base})[0];
 
