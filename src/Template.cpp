f9917b087b30e9e21e87937a078cdd17de5d9fba
Brett Bowman <bnbowman@gmail.com>
2015-10-29 19:02:18 -0700

    Fixes for Deletion-mutation related boundary problems

--- src/Template.cpp
+++ src/Template.cpp
@@ -113,7 +113,9 @@ boost::optional<Mutation> Template::Mutate(const Mutation& mut)
 
     // TODO(lhepler): The following should never happen, but does.
     //     find the root cause, fix it, and nuke this line
-    if (mutStart_ > tpl_.size()) return boost::none;
+    if (mutStart_  >  tpl_.size() ||
+        (mutStart_ == tpl_.size() && mut.Type == MutationType::DELETION))
+        return boost::none;
 
     if (mut.Type == MutationType::INSERTION) {
         if (mutStart_ > 0) mutTpl_[0] = cfg_->Populate({tpl_[mutStart_ - 1].Base, mut.Base})[0];
@@ -170,7 +172,9 @@ void Template::ApplyMutation(const Mutation& mut)
     
     // TODO(lhepler): The following should never happen, but does.
     //     find the root cause, fix it, and nuke this line
-    if (i > tpl_.size()) return;
+    if (i > tpl_.size() ||
+        (i == tpl_.size() && mut.Type == MutationType::DELETION))
+        return;
 
     if (mut.Type == MutationType::INSERTION) {
         if (i > 0) tpl_[i - 1] = cfg_->Populate({tpl_[i - 1].Base, mut.Base})[0];
