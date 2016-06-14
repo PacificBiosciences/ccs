385c1bcecf3c3228c300325deebb0f514ebf9605
Lance Hepler <nlhepler@gmail.com>
2016-06-13 18:33:57 -0700

    refactor setup.py (ninja by default if avail)

--- setup.py
+++ setup.py
@@ -5,9 +5,11 @@ import os
 import os.path
 import sys
 
+from copy import copy
 from distutils import sysconfig
 from distutils.command.clean import clean as dclean
 from distutils.errors import CompileError
+from distutils.util import strtobool
 from setuptools import setup
 from setuptools.dist import Distribution
 from shutil import rmtree
@@ -18,49 +20,122 @@ thisDir = os.path.dirname(os.path.realpath(__file__))
 swigLib = os.path.join(thisDir, "swig", "lib")
 modName = "_ConsensusCore2.so"
 
+def which(program, env=os.environ):
+    def isExe(fpath):
+        return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
+    fpath, fname = os.path.split(program)
+    if fpath:
+        if isExe(program):
+            return program
+    else:
+        for path in env.get("PATH", "").split(os.pathsep):
+            path = path.strip("\"")
+            exe = os.path.join(path, program)
+            if isExe(exe):
+                return exe
+    return None
+
+
 class MyClean(dclean):
     def run(self):
         rmtree(swigLib, ignore_errors=True)
         dclean.run(self)
 
+
 class MyDist(Distribution):
     def has_ext_modules(self):
         return True
 
+
+class CMake(object):
+    def __init__(self, env=os.environ, verbose=False):
+        cmake = env.get("CMAKE_COMMAND", "cmake") or which("cmake", env)
+        if cmake is None:
+            raise RuntimeError("cannot find `cmake` command, "
+                    "please populate CMAKE_COMMAND environment variable")
+        self.build = ["make"]
+        self.env = env
+        self.configure = [cmake]
+        self.definitions = dict()
+        self.generator = None
+
+        rpath = self.env.get("CMAKE_SKIP_RPATH", "")
+        try:
+            rpath = bool(strtobool(rpath))
+        except ValueError:
+            rpath = False
+        if rpath:
+            self.add_definition("CMAKE_SKIP_RPATH", "TRUE")
+
+        verbose = self.env.get("VERBOSE", "{0}".format(verbose))
+        try:
+            verbose = bool(strtobool(verbose))
+        except ValueError:
+            verbose = False
+        self.verbose = verbose
+
+    def add_definition(self, key, value):
+        self.definitions[key] = value
+
+    def add_definition_from_env(self, key, default=None):
+        value = env.get(key, default)
+        if value:
+            self.add_definition(key, value)
+
+    def set_build_type(self, buildType):
+        if buildType not in set(["Release", "Debug", "RelWithDebInfo"]):
+            raise ValueError("CMAKE_BUILD_TYPE must be in (Release, Debug, RelWithDebInfo)")
+        self.add_definition("CMAKE_BUILD_TYPE", buildType)
+
+    def set_generator(self, gen):
+        if gen not in set(["Default", "Ninja"]):
+            raise ValueError("valid generators must be in (default, ninja)")
+        if gen == "Default":
+            gen = None
+            self.build = ["make"]
+            if self.verbose:
+                self.build.append("VERBOSE=1")
+        elif gen == "Ninja":
+            self.ninja = self.env.get("NINJA_COMMAND", None) or which("ninja", self.env)
+            if not self.ninja:
+                raise RuntimeError("cannot find `ninja` command, "
+                        "please populate NINJA_COMMAND environment variable")
+            self.build = [self.ninja]
+            if self.verbose:
+                self.build.append("-v")
+        self.generator = gen
+
+    def __call__(self, directory):
+        configure = copy(self.configure)
+        if self.generator:
+            configure.append("-G{0}".format(self.generator))
+        for k, v in self.definitions.iteritems():
+            configure.append("-D{0}={1}".format(k, v))
+        configure.append(directory)
+        print("Configuring with command `{0}`".format(" ".join(configure)), file=sys.stderr)
+        retcode = Popen(configure, cwd=buildDir, env=env).wait()
+        if retcode != 0:
+            raise RuntimeError("failed to configure the project")
+        print("Building with command `{0}`".format(" ".join(self.build)), file=sys.stderr)
+        retcode = Popen(self.build, cwd=buildDir).wait()
+        if retcode != 0:
+            raise RuntimeError("failed to build the project")
+
+
+# build the library if we haven't already
 if not os.path.exists(os.path.join(swigLib, modName)):
     buildDir = mkdtemp()
     try:
         env = os.environ.copy()
-        cmake = env.get("CMAKE_COMMAND", "cmake")
-        boost = env.get("Boost_INCLUDE_DIRS", None)
-        pyinc = env.get("PYTHON_INCLUDE_DIRS", None)
-        rpath = env.get("CMAKE_SKIP_RPATH", None)
-        swig = env.get("SWIG_COMMAND", None)
-        cmds = [cmake,
-                "-DCMAKE_BUILD_TYPE=RelWithDebInfo",
-                "-DPYTHON_SWIG=1",
-                "-DPYTHON_EXECUTABLE={0}".format(sys.executable)]
-        if boost is not None:
-            cmds.append("-DBoost_INCLUDE_DIRS={0}".format(boost))
-        if pyinc is not None:
-            cmds.append("-DPYTHON_INCLUDE_DIRS={0}".format(pyinc))
-        if rpath is not None:
-            cmds.append("-DCMAKE_SKIP_RPATH=TRUE")
-        if swig is not None:
-            cmds.append("-DSWIG_COMMAND={0}".format(swig))
-        cmds.append(thisDir)
-        print("Running command {0}".format(" ".join(cmds)), file=sys.stderr)
-        retcode = Popen(cmds, cwd=buildDir, env=env).wait()
-        if (retcode != 0):
-            raise RuntimeError("failed to configure the ConsensusCore2 with CMake!")
-        verbose = env.get("VERBOSE", None)
-        cmds = ["make"]
-        if verbose is not None:
-            cmds.append("VERBOSE=1")
-        print("Running command {0}".format(" ".join(cmds)), file=sys.stderr)
-        retcode = Popen(cmds, cwd=buildDir).wait()
-        if (retcode != 0):
-            raise RuntimeError("failed to compile or link ConsensusCore2!")
+        cmake = CMake(env)
+        cmake.add_definition_from_env("Boost_INCLUDE_DIRS")
+        cmake.add_definition_from_env("PYTHON_INCLUDE_DIRS")
+        cmake.add_definition_from_env("SWIG_COMMAND")
+        cmake.add_definition("PYTHON_SWIG", "1")
+        cmake.add_definition("PYTHON_EXECUTABLE", sys.executable)
+        cmake.set_build_type("RelWithDebInfo")
+        cmake.set_generator("Ninja" if which("ninja", env) else "Default")
+        cmake(thisDir)
     finally:
         rmtree(buildDir)
 
