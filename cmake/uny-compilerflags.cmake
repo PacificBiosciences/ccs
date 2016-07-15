a621ed199cc159bdc91a1a9d39e874414482d259
Armin Töpfer <armin.toepfer@gmail.com>
2016-07-15 01:32:46 -0700

    Add sanitizer flags
    Can be activated with -DSANITIZE=T

--- cmake/uny-compilerflags.cmake
+++ cmake/uny-compilerflags.cmake
@@ -2,14 +2,33 @@
 include(CheckCXXCompilerFlag)
 
 # shared CXX flags for all source code & tests
-if (MSVC)
-    set(UNY_FLAGS "/Wall")
-else()
-    set(UNY_FLAGS "-std=c++11 -Wall -Wextra -Wno-unused-parameter -Wno-unused-variable")
-endif()
+set(UNY_FLAGS "-std=c++11 -Wall -Wextra -Wno-unused-parameter -Wno-unused-variable")
 
 # NOTE: quash clang warnings w/ Boost
 check_cxx_compiler_flag("-Wno-unused-local-typedefs" HAS_NO_UNUSED_LOCAL_TYPEDEFS)
 if(HAS_NO_UNUSED_LOCAL_TYPEDEFS)
     set(UNY_FLAGS "${UNY_FLAGS} -Wno-unused-local-typedefs")
+endif()
+
+# Coverage settings
+set(UNY_DEBUG_FLAGS "${UNY_FLAGS} --coverage -fprofile-arcs -ftest-coverage")
+
+# Extra testing that will lead to longer compilation times!
+if (SANITIZE)
+    # AddressSanitizer is a fast memory error detector
+    set(UNY_DEBUG_FLAGS "${UNY_DEBUG_FLAGS} -fsanitize=address -fno-omit-frame-pointer -fno-optimize-sibling-calls")
+
+    # Clang Thread Safety Analysis is a C++ language extension which warns about 
+    # potential race conditions in code.
+    set(UNY_DEBUG_FLAGS "${UNY_DEBUG_FLAGS} -Wthread-safety")
+
+    # ThreadSanitizer is a tool that detects data races
+    set(UNY_DEBUG_FLAGS "${UNY_DEBUG_FLAGS} -fsanitize=thread")
+
+    # MemorySanitizer is a detector of uninitialized reads.
+    set(UNY_DEBUG_FLAGS "${UNY_DEBUG_FLAGS} -fsanitize=memory")
+
+    # UndefinedBehaviorSanitizer is a fast undefined behavior detector.
+    set(UNY_DEBUG_FLAGS "${UNY_DEBUG_FLAGS} -fsanitize=undefined")
+    MESSAGE(${UNY_DEBUG_FLAGS})
 endif()
