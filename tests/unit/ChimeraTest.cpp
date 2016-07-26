b2ecb55411f54642bfac63785cc7cd8294b2dbea
Armin Töpfer <atoepfer@pacificbiosciences.com>
2016-07-26 10:27:20 +0200

    Split chimera tests in fast and extensive
    This is a cherry pick of https://github.com/PacificBiosciences/pbchimera/pull/5; the only difference, we only run extensive if -DEXTENSIVE_TESTING=ON

--- tests/unit/ChimeraTest.cpp
+++ tests/unit/ChimeraTest.cpp
@@ -1,4 +1,3 @@
-
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
@@ -11,13 +10,55 @@
 
 using namespace PacBio::Chimera;
 
-const std::string FILENAME = tests::DataDir + "/test.fasta";
+const std::string FILENAME1 = tests::DataDir + "/chimera_minimal.fasta";
+const std::string FILENAME2 = tests::DataDir + "/chimera_extensive.fasta";
 
-TEST(ChimeraLabeler, EndToEnd)
+TEST(ChimeraLabeler, MinimalEndToEnd)
 {
     using namespace seqan;
 
-    SeqFileIn inputHandle(FILENAME.c_str());
+    SeqFileIn inputHandle(FILENAME1.c_str());
+
+    // Parse the records
+    StringSet<CharString> ids;
+    StringSet<Dna5String> seqs;
+    readRecords(ids, seqs, inputHandle);
+
+    // Declare the vectors we'll use to actually perform the Chimera-labeling
+    std::vector<std::string> idList;
+    std::vector<Dna5String> seqList;
+
+    for (size_t i = 0; i < length(ids); ++i)
+    {
+        idList.push_back(toCString(static_cast<CharString>(ids[i])));
+        seqList.push_back(static_cast<Dna5String>(seqs[i]));
+    }
+
+    // Label the Records
+    ChimeraLabeler chimeraLabeler;
+    const auto& labels = chimeraLabeler.LabelChimeras(idList, seqList);
+
+    // Expect 4 labels
+    EXPECT_EQ(labels.size(), 4);
+
+    // Expect the first 2 (non-Chimeric) sequences to have no score
+    EXPECT_EQ(labels[0].score, -1.0);
+    EXPECT_EQ(labels[1].score, -1.0);
+
+    // Expect the next (homologous) sequence to have a low, non-zero score
+    EXPECT_LT(labels[2].score, 1.0);
+    EXPECT_GT(labels[2].score, 0.0);
+
+    // Expect the last (Chimeric) sequence to have a high score
+    EXPECT_GT(labels[3].score, 1.0);
+}
+
+#if EXTENSIVE_TESTING
+TEST(ChimeraLabeler, ExtensiveEndToEnd)
+{
+    using namespace seqan;
+
+    SeqFileIn inputHandle(FILENAME2.c_str());
     
     // Parse the records
     StringSet<CharString> ids;
@@ -55,3 +96,4 @@ TEST(ChimeraLabeler, EndToEnd)
     EXPECT_GT(labels[8].score, 1.0);
     EXPECT_GT(labels[9].score, 1.0);
 }
+#endif
