ed1d8733c626309050bcc1b4ad5cde154be51216
Armin Töpfer <atoepfer@pacificbiosciences.com>
2016-07-27 14:00:35 +0200

    Minimize uny targets
    Do not build binaries and tests for ConsensusCore2 target

--- setup.py
+++ setup.py
@@ -130,6 +130,8 @@ class MyBuildExt(build_ext):
         cmake.add_definition_from_env("SWIG_COMMAND")
         cmake.add_definition_from_env("CMAKE_BUILD_TYPE", "RelWithDebInfo")
         cmake.add_definition("PYTHON_SWIG", "1")
+        cmake.add_definition("UNY_build_tests", "0")
+        cmake.add_definition("UNY_build_bin", "0")
         cmake.add_definition("PYTHON_EXECUTABLE", sys.executable)
         cmake.set_generator("Ninja" if which("ninja", env) else "Default")
         targets = ["_ConsensusCore2"]
