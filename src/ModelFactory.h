PacificBiosciences/ConsensusCore2

support loading models from json files:
 - introduce model forms for each of Marginal, Snr, PwSnrA, and PwSnr
 - prototype automatic counterWeight calculation (disabled for now)
 - tests for these model forms, including loading params concordant
   with- and tests for correctness against their compiled analogs

src/ModelFactory.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2011-2016, Pacific Biosciences of California, Inc.
+// Copyright (c) 2015-2016, Pacific Biosciences of California, Inc.
 //
 // All rights reserved.
 //
@@ -33,12 +33,18 @@
 // OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
 // SUCH DAMAGE.
 
+// Author: Lance Hepler
+
 #pragma once
 
 #include <map>
 #include <memory>
 #include <set>
 #include <string>
+
+#include <boost/optional.hpp>
+
+#include <pacbio/consensus/Exceptions.h>
 
 namespace PacBio {
 namespace Consensus {
@@ -52,32 +58,42 @@ class ModelConfig;
 class ModelCreator
 {
 public:
-    ModelCreator(const std::set<std::string>& names);
     virtual ~ModelCreator() {}
     virtual std::unique_ptr<ModelConfig> Create(const SNR&) const = 0;
-};
-
-template <typename T>
-class ModelCreatorImpl : public ModelCreator
-{
-public:
-    ModelCreatorImpl<T>(const std::set<std::string>& names) : ModelCreator(names) {}
-    virtual std::unique_ptr<ModelConfig> Create(const SNR& snr) const
-    {
-        return std::unique_ptr<ModelConfig>(new T(snr));
-    }
 };
 
 class ModelFactory
 {
 public:
     static std::unique_ptr<ModelConfig> Create(const std::string& name, const SNR&);
-    static bool Register(const std::string& name, ModelCreator* ctor);
-    static std::set<std::string> SupportedChemistries();
+    static bool Register(const std::string& name, std::unique_ptr<ModelCreator>&& ctor);
+    static boost::optional<std::string> Resolve(const std::string& name);
+    static std::set<std::string> SupportedModels();
 
 private:
-    static std::map<std::string, ModelCreator*>& CreatorTable();
+    static std::map<std::string, std::unique_ptr<ModelCreator>>& CreatorTable();
 };
+
+template <typename T>
+class ModelCreatorImpl : public ModelCreator
+{
+public:
+    ModelCreatorImpl<T>() {}
+    ModelCreatorImpl<T>(const std::set<std::string>& names)
+    {
+        for (const std::string& name : names)
+            if (!ModelFactory::Register(name + "::Compiled",
+                                        std::unique_ptr<ModelCreator>(new ModelCreatorImpl<T>())))
+                throw DuplicateModel(name);
+    }
+
+    virtual std::unique_ptr<ModelConfig> Create(const SNR& snr) const
+    {
+        return std::unique_ptr<ModelConfig>(new T(snr));
+    }
+};
+
+boost::optional<std::string>& ModelOverride();
 
 #define REGISTER_MODEL(cls) \
 private:                    \

src/ModelFormFactory.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2011-2016, Pacific Biosciences of California, Inc.
+// Copyright (c) 2016, Pacific Biosciences of California, Inc.
 //
 // All rights reserved.
 //
@@ -33,58 +33,66 @@
 // OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
 // SUCH DAMAGE.
 
-#pragma once
+// Author: Lance Hepler
 
-#include <stdexcept>
+#include <map>
 #include <string>
+#include <utility>
 
-#include <pacbio/consensus/State.h>
+#include <boost/property_tree/json_parser.hpp>
+#include <boost/property_tree/ptree.hpp>
+
+#include <pacbio/consensus/Exceptions.h>
+
+#include "ModelFactory.h"
+#include "ModelFormFactory.h"
 
 namespace PacBio {
 namespace Consensus {
-
-class StateError : public std::runtime_error
+std::map<std::string, ModelFormCreator*>& ModelFormFactory::CreatorTable()
 {
-public:
-    StateError(State state, const std::string& msg) : std::runtime_error(msg), state_(state) {}
-    State WhatState() const { return state_; }
-    virtual const char* what() const noexcept { return std::runtime_error::what(); }
-private:
-    State state_;
-};
+    static std::map<std::string, ModelFormCreator*> tbl;
+    return tbl;
+}
 
-class TemplateTooSmall : public StateError
+bool ModelFormFactory::LoadModel(const std::string& path)
 {
-public:
-    TemplateTooSmall() : StateError(State::TEMPLATE_TOO_SMALL, "Template too short!") {}
-};
+    using boost::property_tree::ptree;
+    using boost::property_tree::read_json;
 
-class AlphaBetaMismatch : public StateError
-{
-public:
-    AlphaBetaMismatch() : StateError(State::ALPHA_BETA_MISMATCH, "Alpha/beta mismatch!") {}
-};
+    ptree pt;
 
-class ChemistryNotFound : public std::runtime_error
-{
-public:
-    ChemistryNotFound(const std::string& name)
-        : std::runtime_error(std::string("chemistry not found: '") + name + "'")
-    {
+    try {
+        read_json(path, pt);
+        // verify we're looking at consensus model parameters
+        std::string version = pt.get<std::string>("ConsensusModelVersion");
+        if (version != "3.0.0") return false;
+    } catch (boost::property_tree::ptree_error) {
+        return false;
     }
-};
 
-class DuplicateModel : public std::runtime_error
+    const std::string chemistry = pt.get<std::string>("ChemistryName");
+    const std::string form = pt.get<std::string>("ModelForm");
+
+    const auto tbl = CreatorTable();
+    const auto it = tbl.find(form);
+
+    if (it == tbl.end()) return false;
+
+    const std::string name = chemistry + "::" + form + "::" + "FromFile";
+
+    try {
+        return ModelFactory::Register(name, it->second->LoadParams(pt));
+    } catch (DuplicateModel& e) {
+        return false;
+    } catch (MalformedModelFile& e) {
+        return false;
+    }
+}
+
+bool ModelFormFactory::Register(const std::string& form, ModelFormCreator* ctor)
 {
-public:
-    DuplicateModel(const std::string& name) : std::runtime_error("duplicate model: '" + name + "'") {}
-};
-
-class MalformedModelFile : public std::runtime_error
-{
-public:
-    MalformedModelFile() : std::runtime_error("malformed model!") {}
-};
-
-}  // namespace Consensus
-}  // namespace PacBio
+    return CreatorTable().insert(std::make_pair(form, ctor)).second;
+}
+}
+}

src/models/MarginalModelForm.cpp
@@ -1,0 +1,228 @@
+// Copyright (c) 2014-2015, Pacific Biosciences of California, Inc.
+//
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted (subject to the limitations in the
+// disclaimer below) provided that the following conditions are met:
+//
+//  * Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+//
+//  * Redistributions in binary form must reproduce the above
+//    copyright notice, this list of conditions and the following
+//    disclaimer in the documentation and/or other materials provided
+//    with the distribution.
+//
+//  * Neither the name of Pacific Biosciences nor the names of its
+//    contributors may be used to endorse or promote products derived
+//    from this software without specific prior written permission.
+//
+// NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE
+// GRANTED BY THIS LICENSE. THIS SOFTWARE IS PROVIDED BY PACIFIC
+// BIOSCIENCES AND ITS CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+// WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+// OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL PACIFIC BIOSCIENCES OR ITS
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+// USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+// OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+// SUCH DAMAGE.
+
+// Author: Lance Hepler
+
+#include <cassert>
+#include <cmath>
+#include <memory>
+#include <stdexcept>
+
+#include <pacbio/consensus/Exceptions.h>
+#include <pacbio/consensus/ModelConfig.h>
+#include <pacbio/consensus/Read.h>
+
+#include "../JsonHelpers.h"
+#include "../ModelFactory.h"
+#include "../ModelFormFactory.h"
+#include "../Recursor.h"
+
+namespace PacBio {
+namespace Consensus {
+namespace {
+
+constexpr size_t CONTEXT_NUMBER = 8;
+constexpr size_t OUTCOME_NUMBER = 4;
+
+// fwd decl
+class MarginalModelCreator;
+
+class MarginalModel : public ModelConfig
+{
+public:
+    MarginalModel(const MarginalModelCreator* params, const SNR& snr);
+    std::unique_ptr<AbstractRecursor> CreateRecursor(std::unique_ptr<AbstractTemplate>&& tpl,
+                                                     const MappedRead& mr, double scoreDiff) const;
+    std::vector<TemplatePosition> Populate(const std::string& tpl) const;
+    double ExpectedLLForEmission(MoveType move, uint8_t prev, uint8_t curr,
+                                 MomentType moment) const;
+
+private:
+    const MarginalModelCreator* params_;
+    double counterWeight_;
+};
+
+class MarginalRecursor : public Recursor<MarginalRecursor>
+{
+public:
+    MarginalRecursor(std::unique_ptr<AbstractTemplate>&& tpl, const MappedRead& mr,
+                     double scoreDiff, const MarginalModelCreator* params, double counterWeight);
+
+    static std::vector<uint8_t> EncodeRead(const MappedRead& read);
+    double EmissionPr(MoveType move, uint8_t emission, uint8_t prev, uint8_t curr) const;
+    double UndoCounterWeights(size_t nEmissions) const;
+
+private:
+    const MarginalModelCreator* params_;
+    double counterWeight_;
+    double nLgCounterWeight_;
+};
+
+class MarginalModelCreator : public ModelCreator
+{
+    REGISTER_MODELFORM(MarginalModelCreator);
+    friend class MarginalModel;
+    friend class MarginalRecursor;
+
+public:
+    static std::string Name() { return "Marginal"; }
+    MarginalModelCreator(const boost::property_tree::ptree& pt);
+    virtual std::unique_ptr<ModelConfig> Create(const SNR& snr) const
+    {
+        return std::unique_ptr<ModelConfig>(new MarginalModel(this, snr));
+    };
+
+private:
+    double emissionPmf_[3][CONTEXT_NUMBER][OUTCOME_NUMBER];
+    double transitionPmf_[CONTEXT_NUMBER][4];
+    double counterWeight_;
+};
+
+REGISTER_MODELFORM_IMPL(MarginalModelCreator);
+
+MarginalModel::MarginalModel(const MarginalModelCreator* params, const SNR& snr) : params_{params}
+{
+    counterWeight_ = params_->counterWeight_;
+}
+
+std::unique_ptr<AbstractRecursor> MarginalModel::CreateRecursor(
+    std::unique_ptr<AbstractTemplate>&& tpl, const MappedRead& mr, double scoreDiff) const
+{
+    return std::unique_ptr<AbstractRecursor>(
+        new MarginalRecursor(std::forward<std::unique_ptr<AbstractTemplate>>(tpl), mr, scoreDiff,
+                             params_, counterWeight_));
+}
+
+std::vector<TemplatePosition> MarginalModel::Populate(const std::string& tpl) const
+{
+    std::vector<TemplatePosition> result;
+
+    if (tpl.empty()) return result;
+
+    result.reserve(tpl.size());
+
+    // calculate transition probabilities
+    uint8_t prev = detail::TranslationTable[static_cast<uint8_t>(tpl[0])];
+    if (prev > 3) throw std::invalid_argument("invalid character in template!");
+
+    for (size_t i = 1; i < tpl.size(); ++i) {
+        const uint8_t curr = detail::TranslationTable[static_cast<uint8_t>(tpl[i])];
+        if (curr > 3) throw std::invalid_argument("invalid character in template!");
+        const uint8_t ctx = ((prev == curr) << 2) | curr;
+        const auto& params = params_->transitionPmf_[ctx];
+        result.emplace_back(TemplatePosition{
+            tpl[i - 1], prev,
+            params[0],  // match
+            params[1],  // branch
+            params[2],  // stick
+            params[3]   // deletion
+        });
+        prev = curr;
+    }
+    result.emplace_back(TemplatePosition{tpl.back(), prev, 1.0, 0.0, 0.0, 0.0});
+
+    return result;
+}
+
+double MarginalModel::ExpectedLLForEmission(const MoveType move, const uint8_t prev,
+                                            const uint8_t curr, const MomentType moment) const
+{
+    const uint8_t ctx = ((prev == curr) << 2) | curr;
+    double expectedLL = 0;
+    for (size_t i = 0; i < OUTCOME_NUMBER; i++) {
+        double curProb = params_->emissionPmf_[static_cast<uint8_t>(move)][ctx][i];
+        double lgCurProb = std::log(curProb);
+        if (moment == MomentType::FIRST)
+            expectedLL += curProb * lgCurProb;
+        else if (moment == MomentType::SECOND)
+            expectedLL += curProb * (lgCurProb * lgCurProb);
+    }
+    return expectedLL;
+}
+
+MarginalRecursor::MarginalRecursor(std::unique_ptr<AbstractTemplate>&& tpl, const MappedRead& mr,
+                                   double scoreDiff, const MarginalModelCreator* params,
+                                   double counterWeight)
+    : Recursor(std::forward<std::unique_ptr<AbstractTemplate>>(tpl), mr, scoreDiff)
+    , params_{params}
+    , counterWeight_{counterWeight}
+    , nLgCounterWeight_{-std::log(counterWeight_)}
+{
+}
+
+std::vector<uint8_t> MarginalRecursor::EncodeRead(const MappedRead& read)
+{
+    std::vector<uint8_t> result;
+    result.reserve(read.Length());
+
+    for (const char bp : read.Seq) {
+        const uint8_t em = detail::TranslationTable[static_cast<uint8_t>(bp)];
+        if (em > 3) throw std::invalid_argument("invalid character in read!");
+        result.emplace_back(em);
+    }
+
+    return result;
+}
+
+double MarginalRecursor::EmissionPr(MoveType move, uint8_t emission, uint8_t prev,
+                                    uint8_t curr) const
+{
+    assert(move != MoveType::DELETION);
+    const uint8_t ctx = ((prev == curr) << 2) | curr;
+    return params_->emissionPmf_[static_cast<uint8_t>(move)][ctx][emission] * counterWeight_;
+}
+
+double MarginalRecursor::UndoCounterWeights(const size_t nEmissions) const
+{
+    return nLgCounterWeight_ * nEmissions;
+}
+
+MarginalModelCreator::MarginalModelCreator(const boost::property_tree::ptree& pt)
+{
+    try {
+        ReadMatrix<3, CONTEXT_NUMBER, OUTCOME_NUMBER>(emissionPmf_,
+                                                      pt.get_child("EmissionParameters"));
+        ReadMatrix<CONTEXT_NUMBER, 4>(transitionPmf_, pt.get_child("TransitionParameters"));
+        counterWeight_ = pt.get<double>("CounterWeight");
+    } catch (std::invalid_argument& e) {
+        throw MalformedModelFile();
+    } catch (boost::property_tree::ptree_error) {
+        throw MalformedModelFile();
+    }
+}
+
+}  // namespace anonymous
+}  // namespace Consensus
+}  // namespace PacBio

PacificBiosciences/ccs

src/ModelFactory.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2011-2016, Pacific Biosciences of California, Inc.
+// Copyright (c) 2015-2016, Pacific Biosciences of California, Inc.
 //
 // All rights reserved.
 //
@@ -33,12 +33,18 @@
 // OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
 // SUCH DAMAGE.
 
+// Author: Lance Hepler
+
 #pragma once
 
 #include <map>
 #include <memory>
 #include <set>
 #include <string>
+
+#include <boost/optional.hpp>
+
+#include <pacbio/exception/ModelError.h>
 
 namespace PacBio {
 
@@ -59,32 +65,42 @@ using SNR = PacBio::Data::SNR;
 class ModelCreator
 {
 public:
-    ModelCreator(const std::set<std::string>& names);
     virtual ~ModelCreator() {}
     virtual std::unique_ptr<ModelConfig> Create(const SNR&) const = 0;
-};
-
-template <typename T>
-class ModelCreatorImpl : public ModelCreator
-{
-public:
-    ModelCreatorImpl<T>(const std::set<std::string>& names) : ModelCreator(names) {}
-    virtual std::unique_ptr<ModelConfig> Create(const SNR& snr) const
-    {
-        return std::unique_ptr<ModelConfig>(new T(snr));
-    }
 };
 
 class ModelFactory
 {
 public:
     static std::unique_ptr<ModelConfig> Create(const std::string& name, const SNR&);
-    static bool Register(const std::string& name, ModelCreator* ctor);
-    static std::set<std::string> SupportedChemistries();
+    static bool Register(const std::string& name, std::unique_ptr<ModelCreator>&& ctor);
+    static boost::optional<std::string> Resolve(const std::string& name);
+    static std::set<std::string> SupportedModels();
 
 private:
-    static std::map<std::string, ModelCreator*>& CreatorTable();
+    static std::map<std::string, std::unique_ptr<ModelCreator>>& CreatorTable();
 };
+
+template <typename T>
+class ModelCreatorImpl : public ModelCreator
+{
+public:
+    ModelCreatorImpl<T>() {}
+    ModelCreatorImpl<T>(const std::set<std::string>& names)
+    {
+        for (const std::string& name : names)
+            if (!ModelFactory::Register(name + "::Compiled",
+                                        std::unique_ptr<ModelCreator>(new ModelCreatorImpl<T>())))
+                throw PacBio::Exception::DuplicateModel(name);
+    }
+
+    virtual std::unique_ptr<ModelConfig> Create(const SNR& snr) const
+    {
+        return std::unique_ptr<ModelConfig>(new T(snr));
+    }
+};
+
+boost::optional<std::string>& ModelOverride();
 
 #define REGISTER_MODEL(cls) \
 private:                    \

src/ModelFormFactory.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2011-2016, Pacific Biosciences of California, Inc.
+// Copyright (c) 2016, Pacific Biosciences of California, Inc.
 //
 // All rights reserved.
 //
@@ -33,45 +33,67 @@
 // OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
 // SUCH DAMAGE.
 
-#pragma once
+// Author: Lance Hepler
 
-#include <stdexcept>
+#include <map>
 #include <string>
+#include <utility>
 
-#include <pacbio/data/State.h>
+#include <boost/property_tree/json_parser.hpp>
+#include <boost/property_tree/ptree.hpp>
+
+#include <pacbio/exception/ModelError.h>
+
+#include "ModelFactory.h"
+#include "ModelFormFactory.h"
 
 namespace PacBio {
-namespace Exception {
+namespace Consensus {
 
-class StateError : public std::runtime_error
+using ModelError = PacBio::Exception::ModelError;
+
+std::map<std::string, ModelFormCreator*>& ModelFormFactory::CreatorTable()
 {
-public:
-    StateError(PacBio::Data::State state, const std::string& msg)
-        : std::runtime_error(msg)
-        , state_(state)
-    {}
+    static std::map<std::string, ModelFormCreator*> tbl;
+    return tbl;
+}
 
-    PacBio::Data::State WhatState() const { return state_; }
-    virtual const char* what() const noexcept { return std::runtime_error::what(); }
-private:
-    PacBio::Data::State state_;
-};
-
-class TemplateTooSmall : public StateError
+bool ModelFormFactory::LoadModel(const std::string& path)
 {
-public:
-    TemplateTooSmall()
-        : StateError(PacBio::Data::State::TEMPLATE_TOO_SMALL, "Template too short!")
-    {}
-};
+    using boost::property_tree::ptree;
+    using boost::property_tree::read_json;
 
-class AlphaBetaMismatch : public StateError
+    ptree pt;
+
+    try {
+        read_json(path, pt);
+        // verify we're looking at consensus model parameters
+        std::string version = pt.get<std::string>("ConsensusModelVersion");
+        if (version != "3.0.0") return false;
+    } catch (boost::property_tree::ptree_error) {
+        return false;
+    }
+
+    const std::string chemistry = pt.get<std::string>("ChemistryName");
+    const std::string form = pt.get<std::string>("ModelForm");
+
+    const auto tbl = CreatorTable();
+    const auto it = tbl.find(form);
+
+    if (it == tbl.end()) return false;
+
+    const std::string name = chemistry + "::" + form + "::" + "FromFile";
+
+    try {
+        return ModelFactory::Register(name, it->second->LoadParams(pt));
+    } catch (ModelError& e) {
+        return false;
+    }
+}
+
+bool ModelFormFactory::Register(const std::string& form, ModelFormCreator* ctor)
 {
-public:
-    AlphaBetaMismatch()
-        : StateError(PacBio::Data::State::ALPHA_BETA_MISMATCH, "Alpha/beta mismatch!")
-    {}
-};
-
-}  // namespace Exception
-}  // namespace PacBio
+    return CreatorTable().insert(std::make_pair(form, ctor)).second;
+}
+}
+}

src/models/MarginalModelForm.cpp
@@ -1,0 +1,232 @@
+// Copyright (c) 2014-2015, Pacific Biosciences of California, Inc.
+//
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted (subject to the limitations in the
+// disclaimer below) provided that the following conditions are met:
+//
+//  * Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+//
+//  * Redistributions in binary form must reproduce the above
+//    copyright notice, this list of conditions and the following
+//    disclaimer in the documentation and/or other materials provided
+//    with the distribution.
+//
+//  * Neither the name of Pacific Biosciences nor the names of its
+//    contributors may be used to endorse or promote products derived
+//    from this software without specific prior written permission.
+//
+// NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE
+// GRANTED BY THIS LICENSE. THIS SOFTWARE IS PROVIDED BY PACIFIC
+// BIOSCIENCES AND ITS CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+// WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+// OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL PACIFIC BIOSCIENCES OR ITS
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+// USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+// OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+// SUCH DAMAGE.
+
+// Author: Lance Hepler
+
+#include <cassert>
+#include <cmath>
+#include <memory>
+#include <stdexcept>
+
+#include <pacbio/consensus/ModelConfig.h>
+#include <pacbio/data/Read.h>
+#include <pacbio/exception/ModelError.h>
+
+#include "../JsonHelpers.h"
+#include "../ModelFactory.h"
+#include "../ModelFormFactory.h"
+#include "../Recursor.h"
+
+using namespace PacBio::Data;
+
+namespace PacBio {
+namespace Consensus {
+namespace {
+
+using MalformedModelFile = PacBio::Exception::MalformedModelFile;
+
+constexpr size_t CONTEXT_NUMBER = 8;
+constexpr size_t OUTCOME_NUMBER = 4;
+
+// fwd decl
+class MarginalModelCreator;
+
+class MarginalModel : public ModelConfig
+{
+public:
+    MarginalModel(const MarginalModelCreator* params, const SNR& snr);
+    std::unique_ptr<AbstractRecursor> CreateRecursor(std::unique_ptr<AbstractTemplate>&& tpl,
+                                                     const MappedRead& mr, double scoreDiff) const;
+    std::vector<TemplatePosition> Populate(const std::string& tpl) const;
+    double ExpectedLLForEmission(MoveType move, uint8_t prev, uint8_t curr,
+                                 MomentType moment) const;
+
+private:
+    const MarginalModelCreator* params_;
+    double counterWeight_;
+};
+
+class MarginalRecursor : public Recursor<MarginalRecursor>
+{
+public:
+    MarginalRecursor(std::unique_ptr<AbstractTemplate>&& tpl, const MappedRead& mr,
+                     double scoreDiff, const MarginalModelCreator* params, double counterWeight);
+
+    static std::vector<uint8_t> EncodeRead(const MappedRead& read);
+    double EmissionPr(MoveType move, uint8_t emission, uint8_t prev, uint8_t curr) const;
+    double UndoCounterWeights(size_t nEmissions) const;
+
+private:
+    const MarginalModelCreator* params_;
+    double counterWeight_;
+    double nLgCounterWeight_;
+};
+
+class MarginalModelCreator : public ModelCreator
+{
+    REGISTER_MODELFORM(MarginalModelCreator);
+    friend class MarginalModel;
+    friend class MarginalRecursor;
+
+public:
+    static std::string Name() { return "Marginal"; }
+    MarginalModelCreator(const boost::property_tree::ptree& pt);
+    virtual std::unique_ptr<ModelConfig> Create(const SNR& snr) const
+    {
+        return std::unique_ptr<ModelConfig>(new MarginalModel(this, snr));
+    };
+
+private:
+    double emissionPmf_[3][CONTEXT_NUMBER][OUTCOME_NUMBER];
+    double transitionPmf_[CONTEXT_NUMBER][4];
+    double counterWeight_;
+};
+
+REGISTER_MODELFORM_IMPL(MarginalModelCreator);
+
+MarginalModel::MarginalModel(const MarginalModelCreator* params, const SNR& snr) : params_{params}
+{
+    counterWeight_ = params_->counterWeight_;
+}
+
+std::unique_ptr<AbstractRecursor> MarginalModel::CreateRecursor(
+    std::unique_ptr<AbstractTemplate>&& tpl, const MappedRead& mr, double scoreDiff) const
+{
+    return std::unique_ptr<AbstractRecursor>(
+        new MarginalRecursor(std::forward<std::unique_ptr<AbstractTemplate>>(tpl), mr, scoreDiff,
+                             params_, counterWeight_));
+}
+
+std::vector<TemplatePosition> MarginalModel::Populate(const std::string& tpl) const
+{
+    std::vector<TemplatePosition> result;
+
+    if (tpl.empty()) return result;
+
+    result.reserve(tpl.size());
+
+    // calculate transition probabilities
+    uint8_t prev = detail::TranslationTable[static_cast<uint8_t>(tpl[0])];
+    if (prev > 3) throw std::invalid_argument("invalid character in template!");
+
+    for (size_t i = 1; i < tpl.size(); ++i) {
+        const uint8_t curr = detail::TranslationTable[static_cast<uint8_t>(tpl[i])];
+        if (curr > 3) throw std::invalid_argument("invalid character in template!");
+        const uint8_t ctx = ((prev == curr) << 2) | curr;
+        const auto& params = params_->transitionPmf_[ctx];
+        result.emplace_back(TemplatePosition{
+            tpl[i - 1], prev,
+            params[0],  // match
+            params[1],  // branch
+            params[2],  // stick
+            params[3]   // deletion
+        });
+        prev = curr;
+    }
+    result.emplace_back(TemplatePosition{tpl.back(), prev, 1.0, 0.0, 0.0, 0.0});
+
+    return result;
+}
+
+double MarginalModel::ExpectedLLForEmission(const MoveType move, const uint8_t prev,
+                                            const uint8_t curr, const MomentType moment) const
+{
+    const uint8_t ctx = ((prev == curr) << 2) | curr;
+    double expectedLL = 0;
+    for (size_t i = 0; i < OUTCOME_NUMBER; i++) {
+        double curProb = params_->emissionPmf_[static_cast<uint8_t>(move)][ctx][i];
+        double lgCurProb = std::log(curProb);
+        if (moment == MomentType::FIRST)
+            expectedLL += curProb * lgCurProb;
+        else if (moment == MomentType::SECOND)
+            expectedLL += curProb * (lgCurProb * lgCurProb);
+    }
+    return expectedLL;
+}
+
+MarginalRecursor::MarginalRecursor(std::unique_ptr<AbstractTemplate>&& tpl, const MappedRead& mr,
+                                   double scoreDiff, const MarginalModelCreator* params,
+                                   double counterWeight)
+    : Recursor(std::forward<std::unique_ptr<AbstractTemplate>>(tpl), mr, scoreDiff)
+    , params_{params}
+    , counterWeight_{counterWeight}
+    , nLgCounterWeight_{-std::log(counterWeight_)}
+{
+}
+
+std::vector<uint8_t> MarginalRecursor::EncodeRead(const MappedRead& read)
+{
+    std::vector<uint8_t> result;
+    result.reserve(read.Length());
+
+    for (const char bp : read.Seq) {
+        const uint8_t em = detail::TranslationTable[static_cast<uint8_t>(bp)];
+        if (em > 3) throw std::invalid_argument("invalid character in read!");
+        result.emplace_back(em);
+    }
+
+    return result;
+}
+
+double MarginalRecursor::EmissionPr(MoveType move, uint8_t emission, uint8_t prev,
+                                    uint8_t curr) const
+{
+    assert(move != MoveType::DELETION);
+    const uint8_t ctx = ((prev == curr) << 2) | curr;
+    return params_->emissionPmf_[static_cast<uint8_t>(move)][ctx][emission] * counterWeight_;
+}
+
+double MarginalRecursor::UndoCounterWeights(const size_t nEmissions) const
+{
+    return nLgCounterWeight_ * nEmissions;
+}
+
+MarginalModelCreator::MarginalModelCreator(const boost::property_tree::ptree& pt)
+{
+    try {
+        ReadMatrix<3, CONTEXT_NUMBER, OUTCOME_NUMBER>(emissionPmf_,
+                                                      pt.get_child("EmissionParameters"));
+        ReadMatrix<CONTEXT_NUMBER, 4>(transitionPmf_, pt.get_child("TransitionParameters"));
+        counterWeight_ = pt.get<double>("CounterWeight");
+    } catch (std::invalid_argument& e) {
+        throw MalformedModelFile();
+    } catch (boost::property_tree::ptree_error) {
+        throw MalformedModelFile();
+    }
+}
+
+}  // namespace anonymous
+}  // namespace Consensus
+}  // namespace PacBio