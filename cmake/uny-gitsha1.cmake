aa8a79e38d55ac894b6df29b3fe04e10f7c019e4
Lance Hepler <nlhepler@gmail.com>
2016-10-03 11:32:30 -0700

    change the dirty tag to -dirty
    this is to support semantic versioning, which does not
    support "*" inside the build field, only [a-zA-Z-].

--- cmake/uny-gitsha1.cmake
+++ cmake/uny-gitsha1.cmake
@@ -7,7 +7,7 @@ set(__find_git_sha1 YES)
 function(find_git_sha1 _GIT_SHA1)
     find_package(Git QUIET REQUIRED)
     execute_process(COMMAND
-        "${GIT_EXECUTABLE}" "describe" "--always" "--dirty=*"
+        "${GIT_EXECUTABLE}" "describe" "--always" "--dirty=-dirty"
         WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
         RESULT_VARIABLE res
         OUTPUT_VARIABLE out
