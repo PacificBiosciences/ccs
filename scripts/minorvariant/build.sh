c194c371ed4941746b9044aef5d103737ae3496e
Armin Töpfer <atoepfer@pacificbiosciences.com>
2017-01-23 11:28:36 +0100

    Add julietflow synthetical data siv test
    Make sure that an unaligned mix 97:1:1:1 HIV data set (synthetical NL4-3 mutated strains) can be analyzed from scratch and generates a TPR of 1.0

---
+++ scripts/minorvariant/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -euo pipefail
+
+source /mnt/software/Modules/current/init/bash
+module load cram/0.7 samtools
+
+(cd artifacts/blasr/ && for i in *tgz; do tar --overwrite -x -f $i; done)
+if [ ! -f `pwd`/artifacts/blasr/lib/libhdf5_cpp.so.12 ]
+then
+    ln -s /mnt/software/h/hdf5-tools/1.8.16/centos-7/lib/libhdf5_cpp.so `pwd`/artifacts/blasr/lib/libhdf5_cpp.so.12
+fi
+
+export PATH=`pwd`/artifacts/blasr/bin:`pwd`/artifacts/unanimity:$PATH
+set +u
+export LD_LIBRARY_PATH=/mnt/software/h/hdf5-tools/1.8.16/centos-7/lib:`pwd`/artifacts/blasr/lib:$LD_LIBRARY_PATH
+set -u
+
+wdir=$(pwd)
+scripts/cram tests/cram/julietflow.t --xunit-file=${wdir}/juliet-cram.xml
