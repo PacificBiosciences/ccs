e1f9b02a116c210ead7436ff3bc69a5ccb8a70fb
Armin Töpfer <atoepfer@pacificbiosciences.com>
2017-01-27 13:47:35 +0100

    Add S/P2-C2 to juliet
    http://confluence.nanofluidics.com:8090/display/~atoepfer/Analyze+HIV+SB+data

--- src/ErrorEstimates.cpp
+++ src/ErrorEstimates.cpp
@@ -48,6 +48,11 @@ ErrorEstimates::ErrorEstimates(const std::string& chemistry)
         substitution = 0.0007421148 / 3.0;  // 0.0006101725 + 3*4.398076e-05
         deletion = 0.006179274;             // 0.003515625 + 3*0.0008878829
         insertion = 0;
+    } else if (chemistry == "S/P2-C2") {
+        match = 0.9930786;
+        substitution = 0.0005244257 / 3.0;
+        deletion = 0.003791086;
+        insertion = 0;
     } else {
         throw std::runtime_error("Unknown error model");
     }
